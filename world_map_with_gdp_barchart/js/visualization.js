0d0db2d12419c5bb8252670af9b3bcd8380d166a
vandana-vis <71045745+vandana-vis@users.noreply.github.com>
2020-12-01 21:45:15 -0500

    Vandana_UpdatedOnZoomMap_BarAnimation,FactBookCorrection,
    Vandana_UpdatedOnZoomMap_BarAnimation,FactBookCorrection,

--- world_map_with_gdp_barchart/js/visualization.js
+++ world_map_with_gdp_barchart/js/visualization.js
@@ -4,61 +4,91 @@
   const defaultTopics = [
     {
       topic: "Population",
-      colorRange: ["rgb(0, 0, 30)", "rgb(0, 0, 255)"],
+      colorFn: d3
+        .scaleThreshold()
+        .domain([0, 10000000, 50000000, 100000000, 250000000, 5000000000])
+        .range(d3.schemeYlGnBu[7]),
+      densities: ["0", "10M", "50M", "100M", "250M", ">250M"],
     },
     {
       topic: "GDP - per capita",
-      colorRange: ["rgb(30, 0, 0)", "rgb(255, 0, 0)"],
+      colorFn: d3
+        .scaleThreshold()
+        .domain([0, 2000, 5000, 10000, 20000, 35000, 50000, 100000])
+        .range(d3.schemeYlOrRd[9]),
+      densities: [
+        "0",
+        "$2,000",
+        "$5,000",
+        "$10,000",
+        "$20,000",
+        "$35,000",
+        "$50,000",
+        ">$50,000",
+      ],
     },
     {
       topic: "Unemployment rate(%)",
-      colorRange: ["rgb(0, 30, 0)", "rgb(0, 255, 0)"],
+      colorFn: d3
+        .scaleThreshold()
+        .domain([0, 1, 5, 10, 15, 20, 50, 100])
+        .range(d3.schemeYlGn[9]),
+      densities: ["0%", "1%", "5%", "10%", "15%", "20%", "50%", ">50%"],
     },
     {
       topic: "Electricity - production(kWh)",
-      colorRange: ["rgb(0, 30, 30)", "rgb(0, 255, 255)"],
+      colorFn: d3
+        .scaleThreshold()
+        .domain([
+          0,
+          100000000,
+          10000000000,
+          50000000000,
+          100000000000,
+          1000000000000,
+          5000000000000,
+        ])
+        .range(d3.schemeOranges[8]),
+      densities: ["0", "0.1B", "10B", "50B", "100B", "1000B", ">1000B"],
     },
   ];
 
    
   let continent = "All",
-    topic = "Population",
-    maxValue,
-    minValue;
+    topic = "Population";
   
-  const topicColor = d3
-    .scaleLinear()
-    .range(defaultTopics.find((t) => t.topic === topic).colorRange);
-  
-  
-  const margin = { top: 0, right: 0, bottom: 0, left: 0 };
-  const width = 750 - margin.left - margin.right;
+  let topicColor = defaultTopics.find((t) => t.topic === topic).colorFn;
+  let topicDensity = defaultTopics.find((t) => t.topic === topic).densities;
+  const densityLineWidth = 500;
+  const margin = { top: 0, right: 0, bottom: 40, left: 0 };
+  const width = 850 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
-
+  
   const svg = d3
     .select(".world-map")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-    .style("background", "lightgrey")
+    .style("background", "grey")
     .append("g")
     .attr("class", "map");
-  
-   
+     
   const tip = d3
     .tip()
     .attr("class", "d3-tip")
-    .offset([150, 50])
+    .offset([100, 50])
     .html(
       (d) =>
         `<strong>Country: </strong><span class='details'>${
           d.country
         }<br></span><strong>${
           d.topic
-        }: </strong><span class='details'>${parseFloat(d.value).toLocaleString()}</span>`
+        }: </strong><span class='details'>${parseFloat(
+          d.value
+        ).toLocaleString()}</span>`
     );
 
-  //div and div1 are for the tooltip
+  //div tooltip - Vandana
   var div = d3.select("body").append("div")	
       .attr("class", "tooltip")				
       .style("opacity", 0);
@@ -67,7 +97,7 @@
   const projection = d3
     .geoMercator()
     .scale(100)
-    .translate([width / 2.3, height / 1.5]);
+    .translate([width / 2.2, height / 1.5]);
 
   const path = d3.geoPath().projection(projection);
 
@@ -82,6 +112,15 @@
           world.features.map((country) => country.properties.continent)
         ),
       ];
+
+      let countriesIncContinent = {};
+
+      //get countries in continent
+      continents.forEach((c) => {
+        countriesIncContinent[c] = world.features
+          .filter((country) => country.properties.continent === c)
+          .map((country) => country.properties.name);
+      });
       
       const continent_selector = document.getElementById("continent-selector");      
       const topic_selector = document.getElementById("topic-selector");
@@ -114,36 +153,112 @@
       topic_selector.addEventListener("change", function (e) {
         topic = e.target.value;
         //console.log(topic);
-        maxValue = Math.max(...facts.map((f) => f[topic]));
-        minValue = Math.min(...facts.map((f) => f[topic]));
-        topicColor
-          .domain([minValue, maxValue])
-          .range(defaultTopics.find((t) => t.topic === topic).colorRange);
+        topicColor = defaultTopics.find((t) => t.topic === topic).colorFn;
+        topicDensity = defaultTopics.find((t) => t.topic === topic).densities;
         
         updateWorldmap();        
         barcharthigh();
         barchartlow();
       });
-
-      maxValue = Math.max(...facts.map((f) => f[topic]));
-      minValue = Math.min(...facts.map((f) => f[topic]));
-
-      topicColor
-        .domain([minValue, maxValue])
-        .range(defaultTopics.find((t) => t.topic === topic).colorRange);
       
       updateWorldmap();
       
       function updateWorldmap() {
-        
-        svg.select(".countries").remove();
+        svg.selectAll("*").remove();
         svg
+          .append("text")
+          .style("font-size",17)
+          .style("font-weight", "bold")
+          .style("fill", "black")
+          .attr("x", 20)
+          .attr("y", 20)
+          .text(topic + ' (2005)');
+        const densityG = svg
+          .append("g")
+          .attr("class", "density-line")
+          .attr(
+            "transform",
+            `translate(${(width - densityLineWidth) / 2}, ${height - 40})`
+          );
+
+        //topicDensity
+        let rectWidth = Math.floor(densityLineWidth / topicDensity.length);
+        const densityX = d3
+          .scaleBand()
+          .domain(topicDensity)
+          .range([0, densityLineWidth]);
+        const densityXAxis = densityG.append("g").call(d3.axisBottom(densityX));
+        densityXAxis.selectAll("text").attr("dx", -densityX.bandwidth() / 2);
+        densityXAxis.selectAll("line").remove();
+        densityXAxis.selectAll(".domain").remove();
+        const zone = densityG
+          .selectAll("zone")
+          .data(topicDensity.slice(0, -1))
+          .enter()
+          .append("g")
+          .attr("class", ".zone");
+        zone
+          .append("rect")
+          .attr("x", (_, i) => i * rectWidth)
+          .attr("y", 20)
+          .attr("width", rectWidth)
+          .attr("height", 15)
+          .style("fill", (_, i) => topicColor(topicColor.domain()[i]))
+          .on("mouseover", function (d, i) {
+            const ranges = [topicColor.domain()[i], topicColor.domain()[i + 1]];
+            countryMap.selectAll(".country").each(function (d) {
+              if (d) {
+                if (continent !== "All") {
+                  if (countriesIncContinent[continent].includes(d.country)) {
+                    if (
+                      ranges[0] <= parseFloat(d[topic]) &&
+                      parseFloat(d[topic]) <= ranges[1]
+                    ) {
+                      d3.select(this)
+                        .style("stroke", "white")
+                        .style("stroke-width", 2)
+                        .style("opacity", 1);
+                    }
+                  }
+                } else {
+                  if (
+                    ranges[0] <= parseFloat(d[topic]) &&
+                    parseFloat(d[topic]) <= ranges[1]
+                  ) {
+                    d3.select(this)
+                      .style("stroke", "white")
+                      .style("stroke-width", 2)
+                      .style("opacity", 1);
+                  }
+                }
+              }
+            });
+          })
+          .on("mouseout", function (d, i) {
+            countryMap
+              .selectAll(".country")
+              .style("opacity", 1)
+              .style("stroke-width", 0.3)
+              .style("stroke", "white");
+          });
+
+        const countryMap = svg.append("g").attr("class", "country-map");
+        svg.call(d3.zoom().transform, d3.zoomIdentity);
+
+        svg.call(
+          d3.zoom().on("zoom", function () {
+            countryMap.attr("transform", d3.event.transform);
+          })
+        );
+
+        countryMap
           .append("g")
           .attr("class", "countries")
           .selectAll("path")
           .data(world.features)
           .enter()
           .append("path")
+          .attr("class", "country")
           .attr("d", path)
           .style("fill", (d) => {
             let countryInfo;
@@ -151,45 +266,49 @@
               countryInfo = facts.filter(
                 (fact) => fact.Country === d.properties.name
               )[0];
-              
             } else {
               countryInfo = facts.filter(
                 (fact) =>
                   d.properties.continent === continent &&
                   fact.Country === d.properties.name
               )[0];
+            }
+            if (countryInfo) {
+              d["country"] = d.properties.name;
+              d["Population"] = countryInfo["Population"];
+              d["GDP - per capita"] = countryInfo["GDP - per capita"];
+              d["Unemployment rate(%)"] = countryInfo["Unemployment rate(%)"];
+              d["Electricity - production(kWh)"] =
+                countryInfo["Electricity - production(kWh)"];
             }
             return countryInfo ? topicColor(countryInfo[topic]) : "grey";
           })
           .on("mouseover", function (d) {
-            let countryInfo;
-            if (continent === "All") {
-              countryInfo = facts.filter(
-                (fact) => fact.Country === d.properties.name
-              )[0];
-            } else {
-              countryInfo = facts.filter(
-                (fact) =>
-                  d.properties.continent === continent &&
-                  fact.Country === d.properties.name
-              )[0];
-            }
             const tipData = {
               country: d.properties.name_long,
               topic: topic,
-              value: countryInfo ? countryInfo[topic] : null,
+              value: d ? d[topic] : null,
             };
-            countryInfo && tip.show(tipData, this);
-            d3.select(this).style("opacity", 1).style("stroke-width", 2);
+            if (d && continent === "All") {
+              tip.show(tipData, this);
+              d3.select(this).style("opacity", 1).style("stroke-width", 3);
+            }
+            if (
+              d &&
+              countriesIncContinent[continent] &&
+              countriesIncContinent[continent].includes(d.country)
+            ) {
+              tip.show(tipData, this);
+              d3.select(this).style("opacity", 1).style("stroke-width", 3);
+            }
           })
           .on("mouseout", function () {
             tip.hide();
-            d3.select(this).style("opacity", 0.8).style("stroke-width", 1);
+            d3.select(this).style("opacity", 1).style("stroke-width", 0.3);
           })
           .style("stroke", "white")
-          .style("opacity", 0.8)
-          .style("stroke-width", 0.3)
-          ;
+          .style("opacity", 1)
+          .style("stroke-width", 0.6);
       }
       
       //*********SVG1 - BarChart for Top 10 Highest Countries - Vandana**********
@@ -267,11 +386,20 @@
               .attr('fill', 'black')
               .text(xAxisLabelText);
             
+          
           g.selectAll('rect').data(data)
-            .enter().append('rect')
-              .attr('y', d => yScale(yValue(d)))
-              .attr('width', d => xScale(xValue(d)))
-              .attr('height', yScale.bandwidth())             
+              .enter().append('rect')
+                .attr('y', d => yScale(yValue(d)))
+                .attr('width', d => xScale(xValue(0)))
+                .attr('height', yScale.bandwidth()/1.5)
+          
+                //Amination for Bar      
+          g.selectAll("rect")
+              .transition()
+              .duration(800)
+              .attr("y", d => yScale(yValue(d)))
+              .attr("width", d => xScale(xValue(d)))
+              .delay(function(d,i){console.log(i) ; return(i*100)})
 
           g.append('text')
               .attr('class', 'title')
@@ -287,7 +415,8 @@
             d3.select(this).transition()
                   .duration('50')
                   .style("opacity", ".85")
-                  .style("fill","red");
+                  .style("fill","red")
+                  .attr('height', yScale.bandwidth());
             d3.select("svg")
                   .append("g")                  
                   .selectAll("path")
@@ -298,13 +427,13 @@
                   .style("fill", function(f){
                     if(f.properties.name === d["Country"]){
                       //console.log(topic);
-                      return "#fd8d3c";
+                      return "red";
                     }else{
                       return "grey";
                     }                 
                   })
                   .style("stroke", "white")
-                  .style("opacity", 0.8)
+                  .style("opacity", 0.85)
                   .style("stroke-width", 1);
                 div.transition()
 	                	.duration(50)
@@ -316,10 +445,12 @@
           }
           function mouseoutbarcharthigh (d,i) {
             //console.log(d['Country']);
+            
             d3.select(this).transition()
                   .duration('50')
                   .style("opacity", 0.9)
-                  .style("fill","black");
+                  .style("fill","black")
+                  .attr('height', yScale.bandwidth()/1.5);
             d3.select("svg")
                   .append("g")                  
                   .selectAll("path")
@@ -447,14 +578,22 @@
               .attr('y', 30)
               .attr('x', innerWidthlow / 2)
               .attr('fill', 'black')
-              .text(xAxisLabelText);
+              .text(xAxisLabelText)
+              ;
             
           g.selectAll('rect').data(data)
             .enter().append('rect')
               .attr('y', d => yScale(yValue(d)))
-              .attr('width', d => xScale(xValue(d)))
-              .attr('height', yScale.bandwidth())             
-
+              .attr('width', d => xScale(xValue(0)))
+              .attr('height', yScale.bandwidth()/1.5)
+              
+          g.selectAll("rect")
+            .transition()
+            .duration(800)
+            .attr("y", d => yScale(yValue(d)))
+            .attr("width", d => xScale(xValue(d)))
+            .delay(function(d,i){console.log(i) ; return(i*100)})
+          
           g.append('text')
               .attr('class', 'title')
               .attr('y', -10)
@@ -469,7 +608,8 @@
             d3.select(this).transition()
                   .duration('50')
                   .style("opacity", ".8")                  
-                  .style("fill", "Red");
+                  .style("fill", "Red")
+                  .attr('height', yScale.bandwidth());
             d3.select("svg")
                   .append("g")                  
                   .selectAll("path")
@@ -480,7 +620,7 @@
                   .style("fill", function(f){
                     if(f.properties.name === d["Country"]){
                       console.log(topic);
-                      return "#fd8d3c";
+                      return "red";
                     }else{
                       return "grey";
                     }                 
@@ -501,7 +641,8 @@
             d3.select(this).transition()
                   .duration('50')
                   .style("opacity", 0.9)
-                  .style("fill","black");
+                  .style("fill","black")
+                  .attr('height', yScale.bandwidth()/1.5);
             d3.select("svg")
                   .append("g")                  
                   .selectAll("path")
